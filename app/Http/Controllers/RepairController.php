4cfebbfe4a86b43af17abb9ffb0522b33e4b9f84
ipaulygon <paulandrei@ymail.com>
2017-03-11 09:41:07 +0800

    last mock

--- app/Http/Controllers/RepairController.php
+++ app/Http/Controllers/RepairController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class RepairController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $delivery = DeliveryHeader::with('supplier')->where('deliveryIsActive','=',1)->get();
+    	return view('Transaction.receive-delivery',compact('delivery'));
+    }
 }
